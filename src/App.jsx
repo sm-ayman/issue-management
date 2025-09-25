16343569232759d8b6f531b9d5049c15879aaecd
sm_ayman <sultanmdayman@gmail.com>
2025-09-25 11:07:12 +0600

    toggling buttons done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter/Counter";
 import Footer from "./components/Footer/Footer";
@@ -5,11 +6,16 @@ import Navbar from "./components/Navbar/Navbar";
 import ToggleBtns from "./components/ToggleBtns/ToggleBtns";
 
 function App() {
+  const [toggleStatus, setToggleStatus] = useState("All");
+
   return (
     <>
       <Navbar></Navbar>
       <Counter></Counter>
-      <ToggleBtns></ToggleBtns>
+      <ToggleBtns
+        toggleStatus={toggleStatus}
+        setToggleStatus={setToggleStatus}
+      ></ToggleBtns>
       <Footer></Footer>
     </>
   );
--- src/components/ToggleBtns/ToggleBtns.jsx
+++ src/components/ToggleBtns/ToggleBtns.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 
-const ToggleBtns = () => {
+const ToggleBtns = ({ toggleStatus, setToggleStatus }) => {
+  console.log(toggleStatus);
   return (
-    <div className="flex  mt-10 items-center justify-end px-15 font-semibold ">
-      <button className="toggle-btns rounded-s-lg ">All</button>
-      <button className="toggle-btns">Pending</button>
-      <button className="toggle-btns">Submitted</button>
-      <button className="toggle-btns rounded-e-lg">Reviewed</button>
+    <div className="flex  mt-10 items-center justify-center md:justify-end px-15 font-semibold">
+      <button
+        onClick={() => setToggleStatus("All")}
+        className={`toggle-btns rounded-s-lg ${
+          toggleStatus == "All" && "!bg-purple-700 text-white duration-400"
+        }`}
+      >
+        All
+      </button>
+      <button
+        onClick={() => setToggleStatus("Pending")}
+        className={`toggle-btns ${
+          toggleStatus == "Pending" && "!bg-purple-700 text-white duration-400"
+        }`}
+      >
+        Pending
+      </button>
+      <button
+        onClick={() => setToggleStatus("Submitted")}
+        className={`toggle-btns ${
+          toggleStatus == "Submitted" &&
+          "!bg-purple-700 text-white duration-400"
+        }`}
+      >
+        Submitted
+      </button>
+      <button
+        onClick={() => setToggleStatus("Reviewed")}
+        className={`toggle-btns rounded-e-lg ${
+          toggleStatus == "Reviewed" && "!bg-purple-700 text-white duration-400"
+        }`}
+      >
+        Reviewed
+      </button>
     </div>
   );
 };
